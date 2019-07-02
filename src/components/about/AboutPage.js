9de79c66d7cc12eb70bbdcc3464accd124750e62
root <root@DESKTOP-QRLQ7Q8.localdomain>
2019-07-02 22:58:35 +0200

    navBar, main pages components

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const AboutPage = () => (
+	<div>
+		<h2>About</h2>
+		<p>
+			This app uses React, Redux, React Router, and many other helpful
+			libraries.
+		</p>
+	</div>
+);
+
+export default AboutPage;
