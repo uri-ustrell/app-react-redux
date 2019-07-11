3b3723a92540fc76da8c5da20c088706f278683e
root <root@DESKTOP-QRLQ7Q8.localdomain>
2019-07-11 23:03:52 +0200

    first thunk middlware dispatch to handle async

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,40 +5,20 @@ import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 
 class CoursePage extends React.Component {
-	state = {
-		course: {
-			title: "title of course"
-		}
-	};
-
-	handleChange = event => {
-		const course = {
-			...this.state.course.title,
-			title: event.target.value
-		};
-		this.setState({ course });
-	};
-
-	handleSubmit = event => {
-		event.preventDefault();
-		this.props.actions.createCourse(this.state.course);
-	};
+	componentDidMount() {
+		this.props.actions.loadCourses().catch(error => {
+			"loading curses failed" + error;
+		});
+	}
 
 	render() {
 		return (
-			<form onSubmit={this.handleSubmit}>
+			<>
 				<h2>Courses</h2>
-				<h3>Add Course</h3>
-				<input
-					type="text"
-					onChange={this.handleChange}
-					value={this.state.course.title}
-				/>
-				<input type="submit" value="Save" />
 				{this.props.courses.map(course => (
 					<div key={course.title}>{course.title}</div>
 				))}
-			</form>
+			</>
 		);
 	}
 }
