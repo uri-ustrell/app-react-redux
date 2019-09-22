5259edbed60b3f8daca010b114472f717985fa6c
root <root@DESKTOP-QRLQ7Q8.localdomain>
2019-09-22 18:39:42 +0200

    production build

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,6 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "./reducers";
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
-import thunk from "redux-thunk";
-
-export default function configureStore(initialState) {
-	const composeEnhancers =
-		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-	return createStore(
-		rootReducer,
-		initialState,
-		composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
-	);
+//Use CommonJA require bleow so we can dynamicaly import during build tim
+if (process.env.NODE_ENV === "producction") {
+	module.exports = require("./configureStore.prod");
+} else {
+	module.exports = require("./configureStore.dev");
 }
